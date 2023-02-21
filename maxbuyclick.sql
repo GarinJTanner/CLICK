c3daa6e6d611076684dfe7c3783b0b4183f67f9a
Garin J Tanner <32025331+GarinJTanner@users.noreply.github.com>
2023-02-21 12:56:18 -0800

    Update maxbuyclick.sql

--- maxbuyclick.sql
+++ maxbuyclick.sql
@@ -1,9 +1,9 @@
-DROP PROCEDURE IF EXISTS maxbuyclick;
+DROP PROCEDURE IF EXISTS buyclick;
 DELIMITER //
-CREATE PROCEDURE maxbuyclick()
+CREATE PROCEDURE buyclick()
 
 BEGIN
-DECLARE $click_mult,$current_cash,$current_time,$current_spent,$current_mult,$current_thresh,$current_tmult,$maxbuyclick_cnt,$maxbuyclick_cost,$purchase_diff,$purchase_time,$system,$time_case,$total_cash,$total_spent,$total_tmult,$total_time,$total_mult,$wallet LONGTEXT;
+DECLARE $click_mult,$current_cash,$current_time,$current_spent,$current_mult,$current_thresh,$current_tmult,$buyclick_cnt,$buyclick_cost,$purchase_diff,$purchase_time,$system,$time_case,$total_cash,$total_spent,$total_tmult,$total_time,$total_mult,$wallet LONGTEXT;
 
 
 	-- ACTIVE
@@ -45,14 +45,14 @@ WHERE
 
 		   SET $click_mult = (select clickmult from click where active=1),
 			   $current_thresh = (select thresh from click where active=1);
-           SET $maxbuyclick_cnt  = (select floor($total_cash/($click_mult*$current_thresh))),
-			   $maxbuyclick_cost  = (select $maxbuyclick_cnt*($click_mult*$current_thresh));
+           SET $buyclick_cnt  = (select floor($total_cash/($click_mult*$current_thresh))),
+			   $buyclick_cost  = (select $buyclick_cnt*($click_mult*$current_thresh));
 
 	-- SUCCESS --
-case when $maxbuyclick_cnt >= 1 THEN
+case when $buyclick_cnt >= 1 THEN
 
                
-			update click set cspent = cspent + $maxbuyclick_cost, clickmult = clickmult + 1*$maxbuyclick_cnt, cash = (select concat('$',(format($current_cash,2)))) WHERE active = 1;
+			update click set cspent = cspent + $buyclick_cost, clickmult = clickmult + 1*$buyclick_cnt, cash = (select concat('$',(format($current_cash,2)))) WHERE active = 1;
 			
            SET $total_mult = (select sum($current_time*$current_mult)),
 				$current_cash = (select sum($total_tmult-$current_spent)),
@@ -61,7 +61,7 @@ case when $maxbuyclick_cnt >= 1 THEN
 				$total_spent = (select sum(cspent) from click WHERE id!=1),
                 $total_cash = (select sum($total_tmult-$total_spent)),
                 $wallet = CONCAT('$',(FORMAT($total_cash,2))),
-                $system = concat('Purchase successful. ',format($maxbuyclick_cnt,0),' additional click(s) for $',(select format(sum($maxbuyclick_cost),2)),'.');
+                $system = concat('Purchase successful. ',format($buyclick_cnt,0),' additional click(s) for $',(select format(sum($buyclick_cost),2)),'.');
             
 			UPDATE click 
 SET 
@@ -73,7 +73,7 @@ WHERE
     id = 1;           
 
 	-- FAILURE --
-when $maxbuyclick_cnt < 1 or $maxbuyclick_cnt is NULL THEN 											
+when $buyclick_cnt < 1 or $buyclick_cnt is NULL THEN 											
                 SET $current_time = (select timestampdiff(second,created_at, current_timestamp()) from click WHERE active=1),
 					$current_mult = (select mult from click where active=1),
                     $total_cash = (select sum($total_tmult-$total_spent)),
@@ -97,7 +97,7 @@ when $maxbuyclick_cnt < 1 or $maxbuyclick_cnt is NULL THEN
  else begin end;
 end case;
 SELECT 
-    CONCAT('$', FORMAT($maxbuyclick_cost, 2)) AS 'total cost',
+    CONCAT('$', FORMAT($buyclick_cost, 2)) AS 'total cost',
     CONCAT('$', FORMAT($total_cash, 2)) AS 'cash',
     $system AS '';
 
